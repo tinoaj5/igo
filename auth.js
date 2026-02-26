5a825b8659d17263d7eda328e0a2ce244f4d5b96
tinoaj5 <tino.aboujaoude@gmail.com>
2026-02-26 17:50:10 +0100

    Refactor auth.js to use new token and profile keys

--- auth.js
+++ auth.js
@@ -1,17 +1,32 @@
-// auth.js
-const AUTH_TOKEN_KEY = "igo_auth_token";
-const PROFILE_KEY    = "igo_profile";
+// auth.js (NEW)
+(function () {
+  const TK = "igoToken";
+  const PF = "igoProfile";
 
-function getToken(){ return localStorage.getItem(AUTH_TOKEN_KEY); }
-function setToken(t){ localStorage.setItem(AUTH_TOKEN_KEY, t); }
+  function getToken() {
+    return localStorage.getItem(TK) || "";
+  }
+  function setToken(t) {
+    localStorage.setItem(TK, t || "");
+  }
+  function getProfile() {
+    try {
+      return JSON.parse(localStorage.getItem(PF) || "{}");
+    } catch {
+      return {};
+    }
+  }
+  function setProfile(p) {
+    localStorage.setItem(PF, JSON.stringify(p || {}));
+  }
+  function signOut() {
+    localStorage.removeItem(TK);
+    localStorage.removeItem(PF);
+  }
 
-function getProfile(){
-  try { return JSON.parse(localStorage.getItem(PROFILE_KEY) || '{}'); }
-  catch(_){ return {}; }
-}
-function setProfile(p){ localStorage.setItem(PROFILE_KEY, JSON.stringify(p || {})); }
-
-function signOut(){
-  localStorage.removeItem(AUTH_TOKEN_KEY);
-  localStorage.removeItem(PROFILE_KEY);
-}
+  window.getToken = getToken;
+  window.setToken = setToken;
+  window.getProfile = getProfile;
+  window.setProfile = setProfile;
+  window.signOut = signOut;
+})();
